d0c786dbb8b87c351ee89c6ed9411cb166286df3
alangomez900 <alan.gomez@live.com.ar>
2024-05-17 17:20:52 -0300

    setup de proyecto

---
+++ src/main/kotlin/com/gamerly/projectgamerly/ProjectGamerlyApplication.kt
@@ -0,0 +1,11 @@
+package com.gamerly.projectgamerly
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectGamerlyApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjectGamerlyApplication>(*args)
+}
