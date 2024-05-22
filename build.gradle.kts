b5b7eb21068723148bbaa2ac9564d633bc85c0b7
Rodrigo Pavon Gomez <rpavongomez@estudiantes.unsam.edu.ar>
2024-05-21 23:07:12 -0300

    update java version to 21 in build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ group = "com.gamerly"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+	sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
@@ -35,7 +35,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
+		jvmTarget = "21"
 	}
 }
 
