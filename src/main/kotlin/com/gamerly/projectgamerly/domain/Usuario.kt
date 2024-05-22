82f267dc14888a9cd0b1fee7ed21d2f52e689108
Rodrigo Pavon Gomez <rpavongomez@estudiantes.unsam.edu.ar>
2024-05-22 20:12:19 -0300

    creacion parcial usuario

---
+++ src/main/kotlin/com/gamerly/projectgamerly/domain/Usuario.kt
@@ -0,0 +1,21 @@
+package com.gamerly.projectgamerly.domain
+
+class Usuario (
+    var nombre: String,
+    var foto: String,
+    var edad: Int,
+    var sexo: String,
+    var juegosPreferidos: List<String>,
+    var diasPreferidos: List<String>,
+    
+)
+
+/*
+ nombre
+ foto
+ edad
+ sexo
+ juegos preferidos
+ dias preferido
+ horarios preferido
+ */
