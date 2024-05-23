8709340fb53152e50a6410c89e71ac1700299d86
alangomez900 <alan.gomez@live.com.ar>
2024-05-22 21:26:42 -0300

    Creacion de clase reseña

---
+++ src/main/kotlin/com/gamerly/projectgamerly/Resenia.kt
@@ -0,0 +1,5 @@
+package com.gamerly.projectgamerly
+
+class Resenia(var idUsuarioEmisor: Int, var idUsuarioReceptor: Int, var puntaje: Int, var comentario: String) {
+
+}
---
+++ src/main/kotlin/com/gamerly/projectgamerly/bootstrap/bootstrap.kt
@@ -0,0 +1,2 @@
+package com.gamerly.projectgamerly.bootstrap
+
