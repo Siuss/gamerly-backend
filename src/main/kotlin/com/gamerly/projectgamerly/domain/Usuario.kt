69341ce2f7162651e5f408e724711c73d783dfc6
julii0m <julianmesina1234@gmail.com>
2024-05-23 23:42:59 -0300

    intento de implementar persistencia

--- src/main/kotlin/com/gamerly/projectgamerly/domain/Usuario.kt
+++ src/main/kotlin/com/gamerly/projectgamerly/domain/Usuario.kt
@@ -15,6 +15,49 @@ class Usuario (
 
 }
 
+
+
+/*
+suponiendo que usamos jpa para persistir...igual no sabria si esta bien implementado
+
+import java.time.LocalDate
+import javax.persistence.*
+
+@Entity
+@Table(name = "usuarios")
+data class Usuario(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(nullable = false)
+    var nombre: String,
+
+    @Column(nullable = false)
+    var foto: String,
+
+    @Column(name = "fecha_de_nacimiento", nullable = false)
+    var fechaDeNacimiento: LocalDate,
+
+    @Column(nullable = false, unique = true)
+    var email: String,
+
+    @Column(nullable = false)
+    var password: String,
+
+    @ElementCollection
+    @CollectionTable(name = "usuario_juegos_preferidos", joinColumns = [JoinColumn(name = "usuario_id")])
+    @Column(name = "juego_preferido")
+    var juegosPreferidos: List<String> = mutableListOf(),
+
+    @ElementCollection
+    @CollectionTable(name = "usuario_dias_preferidos", joinColumns = [JoinColumn(name = "usuario_id")])
+    @Column(name = "dia_preferido")
+    var diasPreferidos: List<String> = mutableListOf()
+)
+
+* */
+
 /*
  nombre
  foto
