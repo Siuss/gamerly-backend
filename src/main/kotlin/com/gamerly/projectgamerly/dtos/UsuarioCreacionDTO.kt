ad92f5015f3b3b4c6739b1eebab456ff605d00b4
alangomez900 <alan.gomez@live.com.ar>
2024-05-24 17:15:21 -0300

    DTOs de creacion y detalle de usuario

---
+++ src/main/kotlin/com/gamerly/projectgamerly/dtos/UsuarioCreacionDTO.kt
@@ -0,0 +1,7 @@
+package com.gamerly.projectgamerly.dtos
+
+class UsuarioCreacionDTO() {
+    lateinit var nombre: String
+    lateinit var email: String
+    lateinit var password: String
+}
---
+++ src/main/kotlin/com/gamerly/projectgamerly/dtos/UsuarioDetalleDTO.kt
@@ -0,0 +1,30 @@
+package com.gamerly.projectgamerly.dtos
+
+import com.gamerly.projectgamerly.domain.Usuario
+import java.time.LocalDate
+
+class UsuarioDetalleDTO() {
+    var id : Long = 0
+    lateinit var nombre: String
+    lateinit var foto: String
+    lateinit var fechaDeNacimiento: LocalDate
+    lateinit var email: String
+    lateinit var password: String
+    lateinit var juegosPreferidos: List<String>
+    lateinit var diasPreferidos: List<String>
+    var reputacion: Long = 0
+
+    constructor(
+        usuario: Usuario
+    ) : this() {
+        this.id = usuario.id
+        this.nombre = usuario.nombre
+        this.fechaDeNacimiento = usuario.fechaDeNacimiento
+        this.foto = usuario.foto
+        this.email = usuario.email
+        this.password = usuario.password
+        this.juegosPreferidos = usuario.juegosPreferidos
+        this.diasPreferidos = usuario.diasPreferidos
+        this.reputacion = usuario.reputacion
+    }
+}
