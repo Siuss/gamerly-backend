7c495991a585b2ef09335f70e8b05835aa276ac4
alangomez900 <alan.gomez@live.com.ar>
2024-06-02 02:15:04 -0300

    endpoint de busqueda de juegos

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.9.3")
 	//runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
