26d784f2e3a9643e81e0303a7d5dc7a44601e3be
stephanie <stephaniepazos89@gmail.com>
2024-06-20 22:35:01 -0300

    se corrigen validaciones del login

--- src/main/kotlin/com/gamerly/projectgamerly/repos/UserRepository.kt
+++ src/main/kotlin/com/gamerly/projectgamerly/repos/UserRepository.kt
@@ -36,7 +36,7 @@ interface UserRepository : CrudRepository<Usuario, Long>{
     @Query("SELECT r FROM Resenia r WHERE r.idUsuarioReceptor = :userId")
     fun findReseniasByUsuarioId(@Param("userId") userId: Long): List<Resenia>
   
-    @EntityGraph(attributePaths = [" horariosPreferidos","diaFavorito", "plataformas"])
+    @EntityGraph(attributePaths = ["horariosPreferidos","diaFavorito", "plataformas", "juegosPreferidos"])
     fun findByEmailAndPassword(email: String, password: String): Optional<Usuario>
 
     @EntityGraph(attributePaths = ["plataformas"])
