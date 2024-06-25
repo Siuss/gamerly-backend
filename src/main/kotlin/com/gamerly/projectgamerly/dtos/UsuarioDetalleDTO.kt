c4a5f150ffbd90ad53bea9856f5a273752785009
salamida <salamida@fktech.net>
2024-06-24 22:26:01 -0300

    Se completa el uso de parametro 'diasHorariosPreferidos' dentro de 'UsuarioDetalleDTO'.

--- src/main/kotlin/com/gamerly/projectgamerly/dtos/UsuarioDetalleDTO.kt
+++ src/main/kotlin/com/gamerly/projectgamerly/dtos/UsuarioDetalleDTO.kt
@@ -2,6 +2,7 @@ package com.gamerly.projectgamerly.dtos
 
 import com.gamerly.projectgamerly.domain.Resenia
 import com.gamerly.projectgamerly.domain.Usuario
+import com.gamerly.projectgamerly.domain.DiaHorarioPreferido
 import java.time.LocalDate
 import java.time.Period
 
@@ -13,7 +14,8 @@ class UsuarioDetalleDTO() {
     lateinit var email: String
     lateinit var password: String
     lateinit var juegosPreferidos: List<String>
-//    lateinit var diasHorariosPreferidos: Set<String>
+    /** Se completa el atributo lista 'diasHorariosPreferidos' */
+    lateinit var diasHorariosPreferidos: List<DiaHorarioPreferido>
     lateinit var plataformas: Set<String>
     lateinit var nacionalidad: String
     var reputacion: Long = 0
@@ -31,7 +33,8 @@ class UsuarioDetalleDTO() {
         this.email = usuario.email
         this.password = usuario.password
         this.juegosPreferidos = usuario.juegosPreferidos.map { it.nombre }
-//        this.diasHorariosPreferidos = usuario.diasHorariosPreferidos
+        /** Se completa el atributo lista 'diasHorariosPreferidos' */
+        this.diasHorariosPreferidos = usuario.diasHorariosPreferidos
         this.plataformas = usuario.plataformas.map { it.nombre }.toSet()
         this.nacionalidad = usuario.nacionalidad
         this.amigos = emptyList()
