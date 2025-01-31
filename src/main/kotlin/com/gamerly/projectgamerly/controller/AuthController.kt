a5c752fb1e2590fe6d204bdb5667b24f0245bc6e
Rodrigo Pavon Gomez <rpavongomez@estudiantes.unsam.edu.ar>
2025-01-31 19:21:43 -0300

    add encoder and reconfig createUser

--- src/main/kotlin/com/gamerly/projectgamerly/controller/AuthController.kt
+++ src/main/kotlin/com/gamerly/projectgamerly/controller/AuthController.kt
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@CrossOrigin(origins = ["**"])
+@CrossOrigin(origins = ["*"], methods = [RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS])
 @RequestMapping("/auth")
 class AuthController {
 
