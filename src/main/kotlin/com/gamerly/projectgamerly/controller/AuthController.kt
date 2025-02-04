3a2194b97a27d12041c676d54a383610211ba548
Rodrigo Pavon Gomez <rpavongomez@estudiantes.unsam.edu.ar>
2025-02-03 23:18:39 -0300

    agree spring docs y encoding utf-8

--- src/main/kotlin/com/gamerly/projectgamerly/controller/AuthController.kt
+++ src/main/kotlin/com/gamerly/projectgamerly/controller/AuthController.kt
@@ -5,6 +5,7 @@ import com.gamerly.projectgamerly.dtos.UsuarioCreacionDTO
 import com.gamerly.projectgamerly.dtos.UsuarioDetalleDTO
 import com.gamerly.projectgamerly.security.TokenUtils
 import com.gamerly.projectgamerly.service.AuthService
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
@@ -18,12 +19,14 @@ class AuthController {
     @Autowired lateinit var tokenUtils: TokenUtils
 
     @PostMapping("/login")
+    @Operation(summary = "Login de usuario", description = "Permite el login de un usuario usando jwt como token de autenticación")
     fun loginAuth (@RequestBody authDTO : AuthDTO) : String {
         val user = authService.login(authDTO)
         return tokenUtils.createToken(user.email, user.id)
     }
 
     @PostMapping("/user")
+    @Operation(summary = "Creacion de usuario", description = "Permite crear usuarios a la app")
     fun crear(@RequestBody crendentialUser : UsuarioCreacionDTO) = authService.crearUsuario(crendentialUser)
 
 
